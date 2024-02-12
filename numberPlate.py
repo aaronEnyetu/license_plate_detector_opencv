dec4d0b4e4c975b726d9ee20fe70290b27dd50ce
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-12 12:24:21 -0800

    Converted image to gray scale

--- numberPlate.py
+++ numberPlate.py
@@ -16,3 +16,12 @@ image = imutils.resize(image, width = 500)
 #Display original image
 cv2.imshow('Original Image', image) #here the Original Image is th name of window /// you can give your own suitable name
 cv2.waitKey(0) #till i press anything it will not execute further
+
+
+# NOW Here, convert image to gray scale
+# This is done because it will reduce the dimension, also reduces complexity of image
+# and yeah there are few algorithms like canny, etc which only works on grayscale images
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray Scale Image', gray)
+cv2.waitKey(0)
