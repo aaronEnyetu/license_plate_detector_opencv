5343d7dd15b52b5bf741b0fdbe7b2468fcb6d31e
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-12 12:48:22 -0800

    Identified contours on the image

--- numberPlate.py
+++ numberPlate.py
@@ -39,4 +39,20 @@ edged = cv2.Canny(gray, 170, 200)
 cv2.imshow('Canny Edge', edged)
 cv2.waitKey(0)
 
+#Find the contours based on the image
+cnts, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+#Here cnts is contours which means that it is like the curve joining all the contour points
+#new is hierarchy - relationship
+#RETR_LIST- it retrieves all the contours but doesn't create any parent-child relationship
+#CHAIN_APPROX_SIMPLE - it removes all the redundant points and compress the contour by saving memory
+
+
+#Create a copy of our original image to draw all the contours
+
+image1 = image.copy()
+cv2.drawContours(image1, cnts, -1, (0,255,0), 3) #this values are fixed ## to draw all the contours in an image
+cv2.imshow('Canny after contouring', image1)
+cv2.waitKey(0)
+
 
